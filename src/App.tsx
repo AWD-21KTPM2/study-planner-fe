75a8df8f5dd1056fc948382628f41df44358ca68
Nguyen Duc Tuan <ngductuan203@gmail.com>
2024-12-10 00:06:46 +0700

    config: test commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routers/router'
 
 const App = (): React.ReactElement => {
-  console.log('App3')
+  console.log('App4')
   return <RouterProvider router={router} />
 }
 
