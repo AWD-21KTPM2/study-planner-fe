934f043e5ab645757007a0e738cb525cc314a14d
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-12-29 10:03:31 +0700

    chore: add some image for landing page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,18 +1,27 @@
+import step2Image from './add_task.png'
 import aiSuggestionsImage from './ai_suggestion.jpg'
+import analyticsImage from './analytics.jpg'
 import focusTimerImage from './focus_timer.jpg'
+import personalizedFeedbackImage from './personalized_feedback.jpg'
+import seamlessSchedulingImage from './seamless_scheduling.png'
+import step1Image from './sign_up.jpg'
 import taskManagementImage from './task_management.png'
+import step3Image from './use_ai_feedback.png'
+import userTestimonial1Image from './user1.jpg'
+import userTestimonial2Image from './user2.jpg'
+import userTestimonial3Image from './user3.jpg'
 
 export {
   aiSuggestionsImage,
-  // analyticsImage,
+  analyticsImage,
   focusTimerImage,
-  // personalizedFeedbackImage,
-  // seamlessSchedulingImage,
-  // step1Image,
-  // step2Image,
-  // step3Image,
-  taskManagementImage
-  // userTestimonial1Image,
-  // userTestimonial2Image,
-  // userTestimonial3Image,
+  personalizedFeedbackImage,
+  seamlessSchedulingImage,
+  step1Image,
+  step2Image,
+  step3Image,
+  taskManagementImage,
+  userTestimonial1Image,
+  userTestimonial2Image,
+  userTestimonial3Image
 }
