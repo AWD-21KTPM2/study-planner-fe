4abdfdab1b5ba9020a5997448111511581e8bb86
Nguyen Duc Tuan <ngductuan203@gmail.com>
2025-01-01 14:10:27 +0700

    fix: error to redirect to home -> to login

--- src/utils/apis/auth-apis.util.ts
+++ src/utils/apis/auth-apis.util.ts
@@ -2,20 +2,9 @@ import { JwtRefreshDTO } from '@/types/user.type'
 
 import axiosClient from '../axios-client.util'
 
-export const refreshTokenApi = async (
-  // accessToken: string | null,
-  refreshToken: string | null
-): Promise<JwtRefreshDTO> => {
+export const refreshTokenApi = async (refreshToken: string | null): Promise<JwtRefreshDTO> => {
   try {
-    const response = await axiosClient.post(
-      'user/refresh-token',
-      { refreshToken }
-      // {
-      //   headers: {
-      //     Authorization: `Bearer ${accessToken}`
-      //   }
-      // }
-    )
+    const response = await axiosClient.post('user/refresh-token', { refreshToken })
     return response.data.data
   } catch (error) {
     throw new Error(`Error while getting new access token: ${error}`)
