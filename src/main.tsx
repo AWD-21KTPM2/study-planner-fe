066b38df8cc96becce272fd41d08540ca2bbf7c8
Nguyen Duc Tuan <ngductuan203@gmail.com>
2025-01-05 15:21:49 +0700

    config: update deploy

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import { createRoot } from 'react-dom/client'
 import App from '@/App'
 
 const queryClient = new QueryClient()
+
+console.log('import.meta.env', import.meta.env)
+
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <QueryClientProvider client={queryClient}>
